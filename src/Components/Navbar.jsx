6f7a9a09b3f615be18e5ea8632fc49ccad7ba46c
kevin <kmarpaung430@gmail.com>
2025-03-29 21:02:46 +0700

    add button navbar and home

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,24 @@
 import logo from "../img/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleAddData = () => {
+    navigate("/admin");
+  };
   return (
     <div className="flex justify-between bg-black  text-white">
       <div className="flex items-center">
         <img className="w-20" src={logo} alt="Logo Blog" />
         <h1>CodePolos</h1>
+      </div>
+      <div className="flex items-center mx-10">
+        <button
+          onClick={handleAddData}
+          className="bg-blue-500  rounded-md h-1/2 p-2 items-center"
+        >
+          tambah data
+        </button>
       </div>
     </div>
   );
--- src/Fragments/Main.jsx
+++ src/Fragments/Main.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import CardInformation from "../Components/CardInformation";
+import { useNavigate } from "react-router-dom";
 
 const Blog = () => {
   const [savedata, setSaveData] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const data = localStorage.getItem("datablog");
@@ -17,20 +19,35 @@ const Blog = () => {
     }
   }, []);
 
+  const handleAddData = () => {
+    navigate("/admin");
+  };
+
   return (
     <div className="md:mx-20">
       <div className="md:flex md:gap-2 md:flex-wrap md:mx-20 mx-25">
-        {savedata.map((dat) => {
-          return (
-            <>
-              <CardInformation
-                judul={dat.judul}
-                subJudul={dat.subJudul}
-                gambar={dat.gambar}
-              ></CardInformation>
-            </>
-          );
-        })}
+        {savedata && savedata.length > 0 ? (
+          savedata.map((dat) => {
+            return (
+              <>
+                <CardInformation
+                  judul={dat.judul}
+                  subJudul={dat.subJudul}
+                  gambar={dat.gambar}
+                ></CardInformation>
+              </>
+            );
+          })
+        ) : (
+          <div className="text-center w-full flexjustify-center my-52">
+            <button
+              onClick={handleAddData}
+              className="bg-blue-500 text-white p-2 rounded-md"
+            >
+              TAMBAH DATA
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
